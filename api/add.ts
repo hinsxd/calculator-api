0c72ce239d0a96b3780aef054c7a4edee2ce7b48
Alvin Li <alvin@wemakeapp.net>
2021-10-22 17:29:45 +0800

    feat: add

--- api/add.ts
+++ api/add.ts
@@ -1,8 +1,12 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 const add = (req: VercelRequest, res: VercelResponse) => {
-  const nums = req.query.nums;
-  res.status(200).send(nums);
+  const nums = (req.query.nums || "") as string;
+  const sum = nums
+    .split(",")
+    .map((num) => parseInt(num, 10))
+    .reduce((s, num) => s + num, 0);
+  res.status(200).json({ sum });
 };
 
 export default add;
